17458c32cccccecbdf075c1843b3e29761063382
Denizayhan04 <denizayhan04qmail.com>
2024-10-05 18:30:39 +0300

    son

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ print(insan[0:4])
 insan[0] = "degisen"
 print(insan)
 insan[2] = [1,2,3]
-print(insan, "\n" , insan[2][2])
+print(insan, "" , insan[2][2])
 print(len(insan))
 print(insan[-1])
 insan.append("yeni")
@@ -103,7 +103,7 @@ def alan (r,pi):
 print(alan(2,3.14))"""
 
 """ print("123","456",sep="/",end="?") """
-
+"""
 def kimlik(isim,soyisim,yas,tc):
     print("isim :{} , soyisim :{} , yaş :{} , tckno :{}".format(isim,soyisim,yas,tc))
 
@@ -112,4 +112,84 @@ def kimlik2(isim="Boş",soyisim="Boş",yas="Boş",tc="Boş"):
 
 kimlik("ali","keser",25,19127754288)
 kimlik2("ali","keser",25)
-kimlik2(soyisim="keser",tc=25)
+kimlik2(soyisim="keser",tc=25)"""
+
+
+"""class Calisan:
+    def __init__(self,isim,maas,tc):
+        self.isim=isim
+        self.maas=maas
+        self.tc=tc
+    
+    def info(self):
+        print("İsim {} maas {} tc {}".format(self.isim,self.maas,self.tc))
+
+    def zam(self):
+        self.maas *= 3
+"""
+
+"""deneme = Calisan("ali",12312,123421324125)
+"""
+
+"""
+print(deneme.isim)        
+deneme.info()
+deneme.zam()
+deneme.info()"""
+
+"""class Calisan2:
+    def __init__(self,isim,maas,tc):
+        self.isim=isim
+        self.__maas=maas
+        self.tc=tc
+    
+    def info(self):
+        print("İsim {} maas {} tc {}".format(self.isim,self.maas,self.tc))
+
+    def zam(self):
+        self.maas *= 3
+
+    def getMaas(self):
+        print(self.__maas)
+    def setMaas(self,yeni):
+        self.__maas = yeni
+
+deneme = Calisan2("ali",12312,123421324125)
+
+deneme.setMaas(74674261)
+deneme.getMaas()
+"""
+"""class Sirket:
+    def __init__(self,isim,tc,maas):
+        self.isim=isim
+        self.tc=tc
+        self.maas=maas
+
+    def info(self):
+        print(self.isim,self.tc,self.maas)
+class Eleman(Sirket):
+    def __init__(self, isim, tc, maas,brans):
+        super().__init__(isim,tc,maas)
+        self.brans = brans
+        print("Sirketin elemanı")
+    def info(self):
+        print("eleman" , self.isim , self.maas , "kadar maaas alır bransı da :", self.brans)
+
+
+ali = Eleman("ali",10129413,21312,"Tr")    
+ali.info()   """
+
+"""def topla(*args):
+    toplam = 0
+    for i in args:
+        toplam += i
+    print(toplam)    
+
+topla(5,74,9,3,1)    """
+"""
+def kimlik(**kwargs):
+    for key,value in kwargs.items():
+        print("anahtarlar" ,key,value)
+
+
+kimlik(isim="ali",yas = 324)"""
